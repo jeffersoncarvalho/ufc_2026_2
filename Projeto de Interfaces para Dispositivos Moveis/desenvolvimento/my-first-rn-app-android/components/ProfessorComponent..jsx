bd19343dfa8c7677ba92ec0157035ea3995df32e
Jefferson de Carvalho Silva <jeffersoncarvalho@gmail.com>
2026-08-17 15:01:16 -0300

    professor component

---
+++ Projeto de Interfaces para Dispositivos Moveis/desenvolvimento/my-first-rn-app-android/components/ProfessorComponent..jsx
@@ -0,0 +1,13 @@
+//crie um componente Professor,com 
+//as seguintes características:
+
+//o professor tem um "name"
+//o professor tem um "title"
+//o professor tem uma "university"
+const ProfessorComponent = () => {
+  return (
+
+  )
+}
+
+export default ProfessorComponent
